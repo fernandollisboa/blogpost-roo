34a301f172deaf5e30411bfcbfb7a34cfaad2051
Fernando Lisboa Costa <fernando.costa@codeminer42.com>
2024-01-11 18:17:53 -0300

    refactor: change spec subject to new instead of call

--- spec/services/bulls/import_spec.rb
+++ spec/services/bulls/import_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 describe Bulls::Import, type: :service do
   describe '#call' do
-    subject(:import_bulls) { described_class.new.call(file_path) }
+    subject(:import_bulls) { described_class.new }
 
     context 'when all information is valid' do
       let(:file_path) { fixture_file_upload('one_bull_with_valid_information.xlsx') }
-
       let(:expected_attributes) do
         {
           registration_code: '000',
@@ -17,11 +16,11 @@ describe Bulls::Import, type: :service do
       end
 
       it 'creates one bull' do
-        expect{ import_bulls }.to change{ Bull.count }.by(1)
+        expect{ import_bulls.call(file_path) }.to change{ Bull.count }.by(1)
       end
 
       it 'creates the bull with the provided information' do
-        import_bulls
+        import_bulls.call(file_path)
 
         expect(Bull.last).to have_attributes(expected_attributes)
       end
@@ -30,8 +29,8 @@ describe Bulls::Import, type: :service do
     context 'when there are multiple bulls in the spreadsheet' do
       let(:file_path) { fixture_file_upload('five_bulls_on_excel.xlsx') }
 
-      it 'creates the same number of bulls' do
-        expect{ import_bulls }.to change{ Bull.count }.by(5)
+      it 'creates the correct number of bulls' do
+	   expect{ import_bulls.call(file_path) }.to change{ Bull.count }.by(5)
       end
     end
   end
